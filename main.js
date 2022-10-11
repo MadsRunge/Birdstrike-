b36fa5691baf3a05b6906938b7b90cef49e0dcdd
madsen10 <91535029+madsen10@users.noreply.github.com>
2022-10-11 09:24:53 +0200

    species chart

---
+++ main.js
@@ -0,0 +1,27 @@
+
+const ctx = document.querySelector('#chart').getContext('2d');
+
+const chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Mourning dove', 'killdeer', 'Barn swallow', 'American kestrel', 'Gulls', 'Horned lark', 'European starling','Sparrows', 'Rock pigeon','Easten meadowlark'],
+        datasets: [{
+            label: "Mourning dove",
+            data: [13396, 8830, 8707, 8104, 7290, 7204, 5809, 4026, 3990, 3826],
+            backgroundColor:["red","green","green","green","green","green","green","green","green","green"]
+        }]
+    },
+    options: {
+        plugins: {
+
+            title: {
+                display: true,
+                text: "Most killed bird species by airplanes (1990-1998"
+            }
+        }
+    }
+}
+
+);
+
+
